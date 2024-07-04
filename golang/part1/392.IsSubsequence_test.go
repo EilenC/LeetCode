4b3c31376c65ffebd22a091e323ee5b5548ab16e
EilenC <chhccht2136@qq.com>
2024-07-04 11:40:40 +0800

    feat: 添加 392 号题目(golang)

---
+++ golang/part1/392.IsSubsequence_test.go
@@ -0,0 +1,39 @@
+package part1
+
+import "testing"
+
+func Test_isSubsequence(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "输入：s = abc, t = ahbgdc",
+			args: args{
+				s: "abc",
+				t: "ahbgdc",
+			},
+			want: true,
+		},
+		{
+			name: "输入：s = axc, t = ahbgdc",
+			args: args{
+				s: "axc",
+				t: "ahbgdc",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSubsequence(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("isSubsequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

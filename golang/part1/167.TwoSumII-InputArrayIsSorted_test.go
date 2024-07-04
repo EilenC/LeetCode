726d24a573154bc6b2ad56c0fcf4369662af60cc
EilenC <chhccht2136@qq.com>
2024-07-05 00:33:41 +0800

    feat: 添加 167 号题目(golang)

---
+++ golang/part1/167.TwoSumII-InputArrayIsSorted_test.go
@@ -0,0 +1,50 @@
+package part1
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_twoSum2(t *testing.T) {
+	type args struct {
+		numbers []int
+		target  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "输入：numbers = [2,7,11,15], target = 9",
+			args: args{
+				numbers: []int{2, 7, 11, 15},
+				target:  9,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "输入：numbers = [2,3,4], target = 6",
+			args: args{
+				numbers: []int{2, 3, 4},
+				target:  6,
+			},
+			want: []int{1, 3},
+		},
+		{
+			name: "输入：numbers = [-1,0], target = -1",
+			args: args{
+				numbers: []int{-1, 0},
+				target:  -1,
+			},
+			want: []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum2(tt.args.numbers, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

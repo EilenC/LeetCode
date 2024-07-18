c36394fed9eea9ca86892d14e670f7a959442e73
EilenC <chhccht2136@qq.com>
2024-07-18 21:07:09 +0800

    feat: 添加 45 号题目(golang)

---
+++ golang/part1/45.JumpGameII_test.go
@@ -0,0 +1,36 @@
+package part1
+
+import "testing"
+
+func Test_jump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "输入: nums = [2,3,1,1,4]",
+			args: args{
+				nums: []int{2, 3, 1, 1, 4},
+			},
+			want: 2,
+		},
+		{
+			name: "输入: nums = [2,3,0,1,4]",
+			args: args{
+				nums: []int{2, 3, 0, 1, 4},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := jump(tt.args.nums); got != tt.want {
+				t.Errorf("jump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
